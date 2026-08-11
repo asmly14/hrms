6765979312e1d14b942889113a3c5f876eae7fa4
asmly14 <asmly14@users.noreply.github.com>
2026-08-11 09:23:16 +0000

    deploy: 134379bef577a15f0d75a00703496aa8dc752586

---
+++ assets/skeleton-C6mXEPW-.js
@@ -0,0 +1,1 @@
+import{j as o}from"./vendor-react-DJbYE_OA.js";import{t as s}from"./index-DbL5D7rm.js";function r({className:t,...e}){return o.jsx("div",{"code-path":"src/components/ui/skeleton.tsx:5:5","data-slot":"skeleton",className:s("bg-accent animate-pulse rounded-md",t),...e})}export{r as S};
